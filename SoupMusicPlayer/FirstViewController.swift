6d95701bd40fd819fe7bfef1b0607b7db3dddedc
amoham37 <amoham811@gmail.com>
2019-02-26 13:31:11 -0600

    Initial Commit

---
+++ SoupMusicPlayer/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  SoupMusicPlayer
+//
+//  Created by Ahmet Mohammed on 2/26/19.
+//  Copyright © 2019 Ahmet. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ SoupMusicPlayer/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  SoupMusicPlayer
+//
+//  Created by Ahmet Mohammed on 2/26/19.
+//  Copyright © 2019 Ahmet. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
